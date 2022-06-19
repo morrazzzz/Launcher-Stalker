50803b858831a68bee121a4dd30621ed31d1d12c
DaniilOzol181 <61312570+DaniilOzol181@users.noreply.github.com>
2022-06-19 18:41:28 +0700

    Initial commit

---
+++ View/ViewModel/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using CoPLauncher.View;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CoPLauncher.View.ViewModel
+{
+    internal class MainViewModel: ViewModelBase
+    {
+        private Page Home = new LaunchPage();
+        private Page Descr = new DescrPage();
+        private Page Settings = new SettingsPage();
+        private Page Graphics = new GraphicsPage();
+        private Page NewMod = new NewModPage();
+        private Page _CurPage = new LaunchPage();
+
+        public Page CurPage
+        {
+            get => _CurPage;
+            set => Set(ref _CurPage, value);
+        }
+
+        public ICommand OpenDescrPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = Descr);
+            }
+        }
+        public ICommand OpenSettingsPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = Settings);
+            }
+        }
+        public ICommand OpenHomePage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = Home);
+            }
+        }
+        public ICommand OpenGraphicsPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = Graphics);
+            }
+        }
+        public ICommand OpenNewModPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = NewMod);
+            }
+        }
+    }
+}
---
+++ View/ViewModel/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoPLauncher.View.ViewModel
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void onPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            onPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
