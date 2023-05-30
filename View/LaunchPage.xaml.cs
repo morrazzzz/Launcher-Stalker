bd61fdb616fea808aef248899feed06d3514b13e
xrMorrazzzz <123486080+morrazzzz@users.noreply.github.com>
2023-05-30 17:28:24 +0300

    Добавил возможнось выбрать часть сталкера
    А также обновил ресурсы

--- View/LaunchPage.xaml.cs
+++ View/LaunchPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -22,6 +24,7 @@ namespace CoPLauncher.View
     public partial class LaunchPage : Page
     {
         bool close_launcher = false;
+        int mode_stalker_start = 0;
 
         public LaunchPage()
         {
@@ -37,6 +40,18 @@ namespace CoPLauncher.View
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
             close_launcher = true;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selectionItem = ComboBox_mode_Stalker.SelectedIndex;
+
+            if (selectionItem == 0)
+                mode_stalker_start = 1;
+            else if (selectionItem == 1)
+                mode_stalker_start = 2;  
+            else if (selectionItem == 2)
+                mode_stalker_start = 3;
         }
     }
 }
